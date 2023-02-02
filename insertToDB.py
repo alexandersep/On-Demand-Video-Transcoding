71c67b5693bc5e2f79e877f8b35de558f54f3d7c
alexander <sepelena@tcd.ie>
2023-02-02 10:14:47 +0000

    Alternations to Fioons code n insertToDB, created helper functions and cleaned up code excessive code reusage, potentially broken implementation haven't tested it

--- insertToDB.py
+++ insertToDB.py
@@ -1,51 +1,72 @@
-import sqlite3
-import re
+import sqlite3, re, os
 
 acceptedResolutions = ["640:480", "1280:720","1920:1080","2560:1440","3840:2160"]
-acceptedCodecs = ["I'll do this later"]
+acceptedCodecs = ["H264"]
 
-addingFiles = True
-while(addingFiles):
-    currentlyAdding = False
-    addFilePrompt = str(input("Do you wish to add a file?(y/n)"))
-    if addFilePrompt == "y":
-        currentlyAdding = True
-    else:
-        addingFiles = False
+# Ask to add file, and do so if needed 
+# Ensure file conforms to linux naming convention
+def maybe_add_gather_file_and_info():
+    currently_adding = True
+    while(currently_adding):
+        add_file_prompt = str(input("Do you wish to add a file?(y/n) "))
+        if add_file_prompt != "y":
+            break;
+        currently_getting_info = True
+        while (currently_getting_info):
+            maybe_name_file("What is the name of your file?")
+            bool_and_file_scale = is_get_info_on_arr("What resolution is this media? Accepted resolutions:", 
+                                acceptedResolutions, "Error: Forbidden resolution.")
+            if not bool_and_file_scale[0]: 
+                break;
+            bool_and_file_codec = is_get_info_on_arr("What encoding is this media in? Accepted codecs:", 
+                                acceptedCodecs, "Error: Forbidden codec.")
+            if not bool_and_file_codec[0]:
+                break;
+            bool_and_file_output_name = maybe_name_file("What is the output name of your file?", file_name)
+            if not bool_and_file_output_name[0]:
+                break;
+            print(bool_and_file_scale[1], bool_and_file_codec[1], bool_and_file_output_name[1])
+            currently_getting_info = False # end while loop
 
-    while(currentlyAdding):
-        fileName = str(input("What is the name of your file?"))
-        forbidden = True if (re.search("[/><|:&]", fileName)) or (fileName == "/") else False
-        if forbidden:
-            print("Error: Forbidden characters in file name.")
-            currentlyAdding = False
-            break
-    
-        print("What resolution is this media? Accepted resolutions:")
-        print(acceptedResolutions)
-        fileScale = str(input())
-        if(fileScale not in acceptedResolutions):
-            print("Error: Forbidden resolution.")
-            currentlyAdding = False
-            break
-        
-        print("What encoding is this media in? Accepted codeccs:")
-        print(acceptedCodecs)
-        fileCodec = str(input())
-        if(fileCodec not in acceptedCodecs):
-            print("Error: Forbidden codec.")
-            currentlyAdding = False
-            break
+# Generic: Ask to gather information array and
+# if information is not in array error out and erturn False
+# If not erroring, return True
+def is_get_info_on_arr(question, arr, error_str):
+    print(question)
+    print(arr)
+    info = str(intput())
+    if info not in arr:
+        print(error_str)
+        return (False,info)
+    return (True,info)
 
-        fileOutputName = str(input("What is the output name of your file?"))
-        forbidden = True if (re.search("[/><|:&]", fileName)) or (fileName == "/") else False
-        if forbidden:
-            print("Error: Forbidden characters in file name.")
-            currentlyAdding = False
-            break
+# Name a file if conforms to linux naming convention
+# Return False if it does not, and True if it does
+def maybe_name_file(question, file_name):
+    file_name = str(input(question))
+    currently_add_file = is_search_file_forbidden(file_name)
+    if not currently_add_file:
+        return False
+    return True
+    # Naming file logic goes here #
 
-        print(fileName, fileScale, fileCodec, fileOutputName)
-        currentlyAdding = False
+# Given a Boolean if False print error 
+# return the same Boolean
+def is_search_file_forbidden(file_name):
+    search = re.search("/><|:&", file_name) 
+    if search:
+        print("Error: Inputted a character 
+            that does not conform to linux
+            filenaming convention")
+    return search
 
-    else:
-        addingFiles = False
+# Runs ffmpeg command on filen_name specified
+# With accepted_resolution, codecs
+# Will always run with -crf 17, and -preset veryslow
+def run_ffmpeg(file_name, resolution, codec):
+    # Logic to conver file_name to dash separated file_name
+    # and additional meta data e.g. to "file-name-4k.mkv"
+    # os.execute(fmmpeg_command) # Shell command, not Yet implemented
+    return False
+
+maybe_add_gather_file_and_info()
